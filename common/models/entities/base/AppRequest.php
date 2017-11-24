c8c8a27032340e43633deb1154823cf3a7ee2c2f
Aleksandr Hrupin <hrupin.work@yandex.ru>
2017-11-24 21:20:38 +0300

    time push

--- common/models/entities/base/AppRequest.php
+++ common/models/entities/base/AppRequest.php
@@ -2,6 +2,8 @@
 namespace common\models\entities\base;
 
 use yii\web\Request;
+use common\models\forms\base\CityForm;
+use common\models\forms\base\RegionForm;
 
 class AppRequest extends Request
 {
@@ -14,15 +16,42 @@ class AppRequest extends Request
         $lang = str_replace(\Yii::$app->params['frontendHostInfo'], '', $lang);
         $lang = str_replace(\Yii::$app->params['backendHostInfo'], '', $lang);
         $lang = str_replace(\Yii::$app->params['apiHostInfo'], '', $lang);
-        $lang = str_replace('.', '', $lang);
-        $lang_url = isset($lang) ? $lang : null;
-        Languages::setCurrent($lang_url);
+        if(substr($lang, -1) == '.'){
+            $lang = substr($lang, 0, -1);
+        }
+        if(strripos($lang, '.') === false){
+            $lang = str_replace('.', '', $lang);
+            $lang_url = isset($lang) ? $lang : null;
+            Languages::setCurrent($lang_url);
+        }
+        else{
+            $lang = explode('.', $lang);
+            $lang_url = isset($lang[0]) ? $lang[0] : null;
+            Languages::setCurrent($lang_url[0]);
+            if(strripos($lang[1], '_') === false){
+
+            }
+            else{
+                if(RegionForm::findOne(['country_id' => \Yii::$app->params['country'], 'alias' => $lang_url[1]])){
+
+                }
+            }
+        }
+
+
+
     }
 
     protected function resolveRequestUri()
     {
         $this->setLanguageFromUrl();
         return parent::resolveRequestUri();
+    }
+
+    protected function controlLanguages($language){
+        if(Languages::find()->where(['url' => $language])->limit(1)->one()){
+
+        }
     }
 
 }
